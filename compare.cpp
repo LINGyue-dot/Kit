782214cd7d45097d93f7413ffe4c48ec4c7a42a5
LINGyue-dot <1635679571@qq.com>
2020-10-08 13:58:01 +0800

    通讯基本完成

--- compare.cpp
+++ compare.cpp
@@ -6,7 +6,11 @@ compare::compare()
 }
 
 
-// 出牌是否符合规则，符合规则就return true
+/**
+ * @brief compare::correctRules 出牌是否符合规则，符合规则就return true
+ * @param arr1
+ * @return
+ */
 bool compare::correctRules(int arr1[]){
     qDebug()<<"asdasdasd"<<endl;
 
@@ -15,7 +19,12 @@ bool compare::correctRules(int arr1[]){
     return true;
 }
 
- // 比较arr1和arr2的大小，如果arr1大 就return true
+/**
+ * @brief compare::arr1Bigerarr2 比较arr1和arr2的大小，如果arr1大 就return true
+ * @param arr1  要出的牌
+ * @param arr2  当前牌桌上的牌
+ * @return
+ */
 bool compare::arr1Bigerarr2(int arr1[],int arr2[]){
 
 
